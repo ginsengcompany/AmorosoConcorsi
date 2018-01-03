770b6818d240167b61e822db840922cd3ac70493
Antonio Valente <antosaverio.valente@gmail.com>
2018-01-03 10:31:17 +0100

    modifico classe domande rispetto alla nuova modalita con griglia dinamica

--- QuizAmoroso/QuizAmoroso/DataModel/Domande.cs
+++ QuizAmoroso/QuizAmoroso/DataModel/Domande.cs
@@ -10,26 +10,10 @@ namespace QuizAmoroso.DataModel
         public string Codice { get; set; }
         public string id_domanda { get; set; }
         public string Domanda { get; set; }
-        public string A { get; set; }
-        public string B { get; set; }
-        public string C { get; set; }
-        public string D { get; set; }
         public string Risposta { get; set; }
         public List<string> Quesiti { get; set; }
         public string tipo { get; set; }
         public string link { get; set; }
-        public Color colorA { get; set; }
-        public Color colorB { get; set; }
-        public Color colorC { get; set; }
-        public Color colorD { get; set; }
-        public FontAttributes GrassettoA { get; set; }
-        public FontAttributes GrassettoB { get; set; }
-        public FontAttributes GrassettoC { get; set; }
-        public FontAttributes GrassettoD { get; set; }
-        public string SizeA { get; set; }
-        public string SizeB { get; set; }
-        public string SizeC { get; set; }
-        public string SizeD { get; set; }
         public string NumeroRisposte { get; set; }
         public List<Quesiti> lstQuesiti { get; set; }
     }
