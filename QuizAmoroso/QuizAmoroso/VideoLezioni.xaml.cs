2223ee7b54b79257842bcf7bece53577f95c10c4
AlessioRivoli <pleyboy87@hotmail.it>
2018-02-23 15:14:54 +0100

    aggiunto Forms video e pagina VideoLezioni

---
+++ QuizAmoroso/QuizAmoroso/VideoLezioni.xaml.cs
@@ -0,0 +1,23 @@
+﻿using FormsVideoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QuizAmoroso
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VideoLezioni : ContentPage
+    {
+        public VideoLezioni(string urlVideo)
+        {
+            InitializeComponent();
+            videoView.Source = VideoSource.FromUri(urlVideo);
+
+        }
+    }
+}
